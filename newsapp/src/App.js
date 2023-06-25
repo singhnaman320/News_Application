573435630393c07feea3e6d3f3e5fae7040141d1
Naman Kumar Singh <singhnaman320@gmail.com>
2023-06-25 17:48:00 +0530

    Adding News to App.js

--- newsapp/src/App.js
+++ newsapp/src/App.js
@@ -5,12 +5,14 @@ import './App.css';
 // this.props -->props, this.state -->state
 import React, { Component } from 'react'
 import Navbar from './components/Navbar';
+import News from './components/News';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <Navbar/>
+        <News/>
       </div>
     )
   }
