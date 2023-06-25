5c5b7baa0cc9692b2d7ce41dd39630c2fbb3861e
Naman Kumar Singh <singhnaman320@gmail.com>
2023-06-25 15:42:36 +0530

    Adding class based component

--- newsapp/src/App.js
+++ newsapp/src/App.js
@@ -2,16 +2,17 @@ import './App.css';
 
 // Modifying App.js for class based component
 // use "rcc" to getclass based component
+// this.props -->props, this.state -->state
 import React, { Component } from 'react'
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        
+        Hello my first class based component
       </div>
     )
   }
 }
 
-// export default App;
+// =export default App;
