ab176041157e0c8ec478442de7fd11ad39b8bf6c
Naman Kumar Singh <singhnaman320@gmail.com>
2023-06-25 17:47:17 +0530

    Adding News component

---
+++ newsapp/src/components/News.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+// Use "rce --> enter" to get simple class structure
+export class News extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default News
