485699c2c5ae38951dafb5935e6d5804dbdf7c01
Naman Kumar Singh <singhnaman320@gmail.com>
2023-06-25 17:39:25 +0530

    Modify Navbar

--- newsapp/src/components/Navbar.js
+++ newsapp/src/components/Navbar.js
@@ -12,7 +12,7 @@ export class Navbar extends Component {
       <div>
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">Navbar</a>
+                <a class="navbar-brand" href="#">TimesNews</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
                 </button>
@@ -39,10 +39,6 @@ export class Navbar extends Component {
                     <a class="nav-link disabled">Disabled</a>
                     </li>
                 </ul>
-                <form class="d-flex" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
                 </div>
             </div>
         </nav>
