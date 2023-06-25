a349ff1e7733fd7e91bce1c29ddb7c375e5986d7
Naman Kumar Singh <singhnaman320@gmail.com>
2023-06-25 17:44:52 +0530

    Modify Navbar

--- newsapp/src/components/Navbar.js
+++ newsapp/src/components/Navbar.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 // Use "rcep --> enter" to get proper structure with proptypes
-export class Navbar extends Component {
+export class Navbar extends Component {  // Do not change this class to className
   static propTypes = {
 
   }
@@ -10,19 +10,19 @@ export class Navbar extends Component {
   render() {
     return (
       <div>
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="/">TimesNews</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="/">TimesNews</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="/">Home</a>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                        <a className="nav-link active" aria-current="page" href="/">Home</a>
                         </li>
-                        <li class="nav-item">
-                        <a class="nav-link" href="/about">About</a>
+                        <li className="nav-item">
+                        <a className="nav-link" href="/about">About</a>
                         </li>
                     </ul>
                 </div>
