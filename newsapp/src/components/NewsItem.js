2970656bf9c1fe63a54056c5f802bca1f8d400e2
Naman Kumar Singh <singhnaman320@gmail.com>
2023-06-26 14:28:24 +0530

    Modifying NewsItem.js

--- newsapp/src/components/NewsItem.js
+++ newsapp/src/components/NewsItem.js
@@ -2,9 +2,19 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
+
+    // Way to define props in class based components : used Javscript destructuring
+    let{title, description} = this.props; 
     return (
       <div>
-        This is a newsItem component
+        <div class="card" style="width: 18rem;">
+          <img src="..." class="card-img-top" alt="..."/>
+        <div class="card-body">
+          <h5 class="card-title">{title}</h5>
+          <p class="card-text">{description}</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+        </div>
       </div>
     )
   }
