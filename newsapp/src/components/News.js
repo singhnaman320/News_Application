52f84ae33200e498f3fb4768adccb3fc3be93cf3
Naman Kumar Singh <singhnaman320@gmail.com>
2023-06-26 15:19:59 +0530

    Modifying News.js

--- newsapp/src/components/News.js
+++ newsapp/src/components/News.js
@@ -5,9 +5,18 @@ import NewsItem from './NewsItem'
 export class News extends Component {
   render() {
     return (
-      <div>
-        This is a News component
-        <NewsItem title="MyTitle" description="myDescription"/> {/*way to pass value of title and description*/}
+      <div className='container my-3'>
+        <h2>TimesNews - Top Headlines</h2>
+        {/* Provide all three in same row */}
+        <div className="row">
+           {/* Provide one news card in sinle column */}
+          <div className="col-md-4"> 
+            <NewsItem title="MyTitle" description="myDescription"/> {/*way to pass value of title and description*/}
+          </div>
+         
+        </div>
+        <NewsItem/>
+        <NewsItem/>
       </div>
     )
   }
