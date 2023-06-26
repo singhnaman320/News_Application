e88063478ca68eafd25c0799dcd7126b1d5fb19d
Naman Kumar Singh <singhnaman320@gmail.com>
2023-06-26 16:24:34 +0530

    Modifying News.js

--- newsapp/src/components/News.js
+++ newsapp/src/components/News.js
@@ -3,6 +3,70 @@ import NewsItem from './NewsItem'
 
 // Use "rce --> enter" to get simple class structure
 export class News extends Component {
+
+  articles = [
+                {
+                    "source": {
+                        "id": "bbc-sport",
+                        "name": "BBC Sport"
+                    },
+                    "author": null,
+                    "title": "'I was nervous!' Anderson throws first pitch at MLB London Series",
+                    "description": "England cricketer James Anderson throws the ceremonial first pitch of the MLB London Series as St Louis Cardinals play Chicago Cubs at London Stadium.",
+                    "url": "http://www.bbc.co.uk/sport/av/baseball/66010494",
+                    "urlToImage": "https://ichef.bbci.co.uk/news/1024/cpsprodpb/13037/production/_130197877_andersonfirstpitch2.jpg",
+                    "publishedAt": "2023-06-25T08:52:20.9172234Z",
+                    "content": "Watch as England cricketer James Anderson throws the ceremonial first pitch of the MLB London Series as the St Louis Cardinals play the Chicago Cubs at London Stadium.\r\nWatch the MLB London Series ga… [+151 chars]"
+                },
+                {
+                    "source": {
+                        "id": "talksport",
+                        "name": "TalkSport"
+                    },
+                    "author": "Joe Moore",
+                    "title": "Jimmy Anderson outclasses Conor McGregor as England cricket star delivers ceremonial first pitch at MLB...",
+                    "description": "Jimmy Anderson certainly did a lot better than Conor McGregor as the English cricket star delivered a perfect first pitch to open the 2023 London Series. The UFC icon may have once switched the oct…",
+                    "url": "https://talksport.com/sport/us-sports/1477331/jimmy-anderson-conor-mcgregor-first-pitch-london-series-baseball/",
+                    "urlToImage": "https://talksport.com/wp-content/uploads/sites/5/2023/06/OQ-TALKSPORT-ANDERSON-MCGREGOR-BBALL.jpg?strip=all&quality=100&w=1500&h=1000&crop=1",
+                    "publishedAt": "2023-06-24T18:09:10Z",
+                    "content": "Jimmy Anderson certainly did a lot better than Conor McGregor as the English cricket star delivered a perfect first pitch to open the 2023 London Series.\r\nThe UFC icon may have once switched the octa… [+3196 chars]"
+                },
+                {
+                    "source": {
+                        "id": "espn-cric-info",
+                        "name": "ESPN Cric Info"
+                    },
+                    "author": null,
+                    "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
+                    "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
+                    "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
+                    "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
+                    "publishedAt": "2020-04-27T11:41:47Z",
+                    "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
+                },
+                {
+                    "source": {
+                        "id": "espn-cric-info",
+                        "name": "ESPN Cric Info"
+                    },
+                    "author": null,
+                    "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
+                    "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
+                    "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
+                    "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
+                    "publishedAt": "2020-03-30T15:26:05Z",
+                    "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
+                }
+            ]
+
+  constructor(){
+
+    super();
+    console.log("Hello I an constructor from News Component")
+
+    this.state= {};
+  }
+
   render() {
     return (
       <div className='container my-3'>
--- newsapp/src/components/NewsItem.js
+++ newsapp/src/components/NewsItem.js
@@ -6,13 +6,13 @@ export class NewsItem extends Component {
     // Way to define props in class based components : used Javscript destructuring
     let{title, description, imageUrl} = this.props; 
     return (
-      <div>
+      <div className='my-3'>
         <div className="card" style={{width: "18rem"}}>
           <img src={imageUrl} className="card-img-top" alt="..."/>
         <div className="card-body">
           <h5 className="card-title">{title}</h5>
           <p className="card-text">{description}</p>
-          <a href="/" className="btn btn-primary">Go somewhere</a>
+          <a href="/" className="btn btn-sm btn-primary">Read More</a>
         </div>
         </div>
       </div>
