ac9d16e0fd57ed8a09ed85cb5abb021153536d93
Naman Kumar Singh <singhnaman320@gmail.com>
2023-06-27 14:41:51 +0530

    Modifying News.js

--- newsapp/src/components/News.js
+++ newsapp/src/components/News.js
@@ -11,19 +11,6 @@ export class News extends Component {
                         "name": "ESPN Cric Info"
                     },
                     "author": null,
-                    "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-                    "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-                    "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-                    "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-                    "publishedAt": "2020-04-27T11:41:47Z",
-                    "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
-                },
-                {
-                    "source": {
-                        "id": "espn-cric-info",
-                        "name": "ESPN Cric Info"
-                    },
-                    "author": null,
                     "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
                     "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
                     "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
