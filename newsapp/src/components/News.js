f51dc5a2620b44e6128deedd1f5776577c49ca3f
Naman Kumar Singh <singhnaman320@gmail.com>
2023-06-27 14:42:10 +0530

    Modifying News.js

--- newsapp/src/components/News.js
+++ newsapp/src/components/News.js
@@ -4,22 +4,6 @@ import NewsItem from './NewsItem'
 // Use "rce --> enter" to get simple class structure
 export class News extends Component {
 
-  articles = [
-                {
-                    "source": {
-                        "id": "espn-cric-info",
-                        "name": "ESPN Cric Info"
-                    },
-                    "author": null,
-                    "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-                    "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-                    "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-                    "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-                    "publishedAt": "2020-03-30T15:26:05Z",
-                    "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
-                }
-            ]
-
   constructor(){
 
     super();
