e72801d21800f0820f1da3ad2fd327c25260f08d
Naman Kumar Singh <singhnaman320@gmail.com>
2023-06-28 11:31:02 +0530

    Modify Navbar.js

--- newsapp/src/components/Navbar.js
+++ newsapp/src/components/Navbar.js
@@ -21,9 +21,14 @@ export class Navbar extends Component {  // Do not change this class to classNam
                         <li className="nav-item">
                         <a className="nav-link active" aria-current="page" href="/">Home</a>
                         </li>
-                        <li className="nav-item">
-                        <a className="nav-link" href="/about">About</a>
-                        </li>
+                        <li className="nav-item"><a className="nav-link" href="/about">About</a></li>
+                        <li className="nav-item"><a className="nav-link" href="/about">Business</a></li>
+                        <li className="nav-item"><a className="nav-link" href="/about">Entertainment</a></li>
+                        <li className="nav-item"><a className="nav-link" href="/about">General</a></li>
+                        <li className="nav-item"><a className="nav-link" href="/about">Health</a></li>
+                        <li className="nav-item"><a className="nav-link" href="/about">Science</a></li>
+                        <li className="nav-item"><a className="nav-link" href="/about">Sports</a></li>
+                        <li className="nav-item"><a className="nav-link" href="/about">Technology</a></li>
                     </ul>
                 </div>
             </div>
