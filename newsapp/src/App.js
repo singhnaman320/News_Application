b7dbedf3648e4a5a0094f65dc310acff3f0c0325
Naman Kumar Singh <singhnaman320@gmail.com>
2023-06-28 13:52:52 +0530

    Modify App.js

--- newsapp/src/App.js
+++ newsapp/src/App.js
@@ -25,25 +25,25 @@ export default class App extends Component {
               <News pageSize={8} country="in" category="general"/> {/* All categories you can see from News API */}
             </Route>
             <Route path='/business'>
-              <News pageSize={8} country="in" category="general"/>
+              <News pageSize={8} country="in" category="business"/>
             </Route>
             <Route path='/entertainment'>
-              <News pageSize={8} country="in" category="general"/>
+              <News pageSize={8} country="in" category="entertainment"/>
             </Route>
             <Route path='/general'>
               <News pageSize={8} country="in" category="general"/>
             </Route>
             <Route path='/health'>
-              <News pageSize={8} country="in" category="general"/>
+              <News pageSize={8} country="in" category="health"/>
             </Route>
             <Route path='/science'>
-              <News pageSize={8} country="in" category="general"/>
+              <News pageSize={8} country="in" category="science"/>
             </Route>
             <Route path='/sports'>
-              <News pageSize={8} country="in" category="general"/>
+              <News pageSize={8} country="in" category="sports"/>
             </Route>
             <Route path='/technology'>
-              <News pageSize={8} country="in" category="general"/>
+              <News pageSize={8} country="in" category="technology"/>
             </Route>
             
           </Switch>
