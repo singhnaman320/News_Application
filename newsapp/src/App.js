67583a9b1e0f94aeb815b5625a0f3a25dd7d3c41
Naman Kumar Singh <singhnaman320@gmail.com>
2023-06-28 15:30:58 +0530

    Modify App.js

--- newsapp/src/App.js
+++ newsapp/src/App.js
@@ -20,29 +20,29 @@ export default class App extends Component {
           <Navbar/>
           <Switch>
             {/* Key here is used for mounting the given category of news overriding the present one */}
-            <Route exact path='/'> {/* will reach to home */}
+            <Route exact path="/"> {/* will reach to home */}
               <News key='general' pageSize={8} country="in" category="general"/> {/* All categories you can see from News API */}
             </Route>
-            <Route exact path='/business'>
-              <News key='business' pageSize={8} country="in" category="business"/>
+            <Route exact path="/business">
+              <News key="business" pageSize={8} country="in" category="business"/>
             </Route>
-            <Route exact path='/entertainment'>
-              <News key='entertainment' pageSize={8} country="in" category="entertainment"/>
+            <Route exact path="/entertainment">
+              <News key="entertainment" pageSize={8} country="in" category="entertainment"/>
             </Route>
-            <Route exact path='/general'>
-              <News key='general' pageSize={8} country="in" category="general"/>
+            <Route exact path="/general">
+              <News key="general" pageSize={8} country="in" category="general"/>
             </Route>
-            <Route exact path='/health'>
-              <News key='health' pageSize={8} country="in" category="health"/>
+            <Route exact path="/health">
+              <News key="health" pageSize={8} country="in" category="health"/>
             </Route>
-            <Route exact path='/science'>
-              <News key='science' pageSize={8} country="in" category="science"/>
+            <Route exact path="/science">
+              <News key="science" pageSize={8} country="in" category="science"/>
             </Route>
-            <Route exact path='/sports'>
-              <News key='sports' pageSize={8} country="in" category="sports"/>
+            <Route exact path="/sports">
+              <News key="sports" pageSize={8} country="in" category="sports"/>
             </Route>
-            <Route exact path='/technology'>
-              <News key='technology' pageSize={8} country="in" category="technology"/>
+            <Route exact path="/technology">
+              <News key="technology" pageSize={8} country="in" category="technology"/>
             </Route>
             
           </Switch>
--- newsapp/src/components/Navbar.js
+++ newsapp/src/components/Navbar.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 
 // Use "rcep --> enter" to get proper structure with proptypes
@@ -13,22 +12,22 @@ export class Navbar extends Component {  // Do not change this class to classNam
       <div>
         <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
             <div className="container-fluid">
-                <Link className="navbar-brand" to="/">TimesNews</Link>
+                <Link className="navbar-brand" to={'/'}>TimesNews</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                          <Link className="nav-link" aria-current="page" to="/">Home</Link>
+                          <Link className="nav-link" aria-current="page" to={'/'}>Home</Link>
                         </li>
-                        <li className="nav-item"><Link className="nav-link" to="/business">Business</Link></li>
-                        <li className="nav-item"><Link className="nav-link" to="/entertainment">Entertainment</Link></li>
-                        <li className="nav-item"><Link className="nav-link" to="/general">General</Link></li>
-                        <li className="nav-item"><Link className="nav-link" to="/health">Health</Link></li>
-                        <li className="nav-item"><Link className="nav-link" to="/science">Science</Link></li>
-                        <li className="nav-item"><Link className="nav-link" to="/sports">Sports</Link></li>
-                        <li className="nav-item"><Link className="nav-link" to="/technology">Technology</Link></li>
+                        <li className="nav-item"><Link className="nav-link" to={'/business'}>Business</Link></li>
+                        <li className="nav-item"><Link className="nav-link" to={'/entertainment'}>Entertainment</Link></li>
+                        <li className="nav-item"><Link className="nav-link" to={'/general'}>General</Link></li>
+                        <li className="nav-item"><Link className="nav-link" to={'/health'}>Health</Link></li>
+                        <li className="nav-item"><Link className="nav-link" to={'/science'}>Science</Link></li>
+                        <li className="nav-item"><Link className="nav-link" to={'/sports'}>Sports</Link></li>
+                        <li className="nav-item"><Link className="nav-link" to={'/technology'}>Technology</Link></li>
                     </ul>
                 </div>
             </div>
