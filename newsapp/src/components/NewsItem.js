a729641295d624430970bd15061e4770d4295a0c
Naman Kumar Singh <singhnaman320@gmail.com>
2023-06-28 17:26:47 +0530

    Modifying NewsItem.js

--- newsapp/src/components/NewsItem.js
+++ newsapp/src/components/NewsItem.js
@@ -12,7 +12,7 @@ export class NewsItem extends Component {
         <div className="card-body">
           <h5 className="card-title">{title}...</h5>
           <p className="card-text">{description}...</p>
-          <p className="card-text"><small className="text-body-secondary">By {author} on {date}</small></p>
+          <p className="card-text"><small className="text-body-secondary">By {author} on {new Date(date).toGMTString()}</small></p>
           <a href={newsUrl} rel="noreferrer" target='_blank' className="btn btn-sm btn-dark">Read More</a>
         </div>
         </div>
