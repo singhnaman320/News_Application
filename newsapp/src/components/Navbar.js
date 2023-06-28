fc189e01aea676b5db3ebbdc85a020f237de88e9
Naman Kumar Singh <singhnaman320@gmail.com>
2023-06-28 11:48:02 +0530

    Modify Navbar.js

--- newsapp/src/components/Navbar.js
+++ newsapp/src/components/Navbar.js
@@ -19,7 +19,7 @@ export class Navbar extends Component {  // Do not change this class to classNam
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                        <a className="nav-link active" aria-current="page" href="/">Home</a>
+                        <a className="nav-link" aria-current="page" href="/">Home</a>
                         </li>
                         <li className="nav-item"><a className="nav-link" href="/about">About</a></li>
                         <li className="nav-item"><a className="nav-link" href="/about">Business</a></li>
