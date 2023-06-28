8a62991631a9cd791d725181c19e127819d9bea7
Naman Kumar Singh <singhnaman320@gmail.com>
2023-06-28 13:28:00 +0530

    Modify App.js

--- newsapp/src/App.js
+++ newsapp/src/App.js
@@ -6,6 +6,12 @@ import './App.css';
 import React, { Component } from 'react'
 import Navbar from './components/Navbar';
 import News from './components/News';
+import{
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+}from 'react-router-dom'
 
 export default class App extends Component {
   render() {
