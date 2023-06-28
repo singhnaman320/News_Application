1ca4310033f641133c82993bbcee630fe834f133
Naman Kumar Singh <singhnaman320@gmail.com>
2023-06-28 17:46:33 +0530

    Modifying News.js

--- newsapp/src/components/NewsItem.js
+++ newsapp/src/components/NewsItem.js
@@ -8,15 +8,14 @@ export class NewsItem extends Component {
     return (
       <div className='my-3'>
         <div className="card">
+          <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left: '90%', zIndex: '1'}}>{source}</span>
           <img src={!imageUrl ? "https://th.bing.com/th/id/OIP.ZII4BmF6lWq7tgUly_qgRwHaEs?w=261&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7" : imageUrl} className="card-img-top" alt="..."/>
-        <div className="card-body">
-          <h5 className="card-title">{title}
-            <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">{source}</span>
-          </h5>
-          <p className="card-text">{description}</p>
-          <p className="card-text"><small className="text-body-secondary">By {author} on {new Date(date).toGMTString()}</small></p>
-          <a href={newsUrl} rel="noreferrer" target='_blank' className="btn btn-sm btn-dark">Read More</a>
-        </div>
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <p className="card-text"><small className="text-body-secondary">By {author} on {new Date(date).toGMTString()}</small></p>
+            <a href={newsUrl} rel="noreferrer" target='_blank' className="btn btn-sm btn-dark">Read More</a>
+          </div>
         </div>
       </div>
     )
