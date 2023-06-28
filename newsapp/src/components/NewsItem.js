d1bc6848bfc481073c84bfa882aab25c4040326e
Naman Kumar Singh <singhnaman320@gmail.com>
2023-06-28 12:48:47 +0530

    Modify app.js

--- newsapp/src/components/NewsItem.js
+++ newsapp/src/components/NewsItem.js
@@ -7,7 +7,7 @@ export class NewsItem extends Component {
     let{title, description, imageUrl, newsUrl} = this.props; 
     return (
       <div className='my-3'>
-        <div className="card" style={{width: "18rem"}}>
+        <div className="card">
           <img src={!imageUrl ? "https://th.bing.com/th/id/OIP.ZII4BmF6lWq7tgUly_qgRwHaEs?w=261&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7" : imageUrl} className="card-img-top" alt="..."/>
         <div className="card-body">
           <h5 className="card-title">{title}...</h5>
